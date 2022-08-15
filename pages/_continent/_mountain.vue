7224fca1ba6193822395a908091256a4e2080bba
scm-wutyitheint <scm.dott.developer10@gmail.com>
2022-08-15 11:31:32 +0630

    add dynamic pages and nuxt router

---
+++ pages/_continent/_mountain.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <img :src="image" />
+    <h1>Continent: {{ continent }}</h1>
+    <h2>Mountain: {{ mountain }}</h2>
+    <p>Path: {{ $route.path }}</p>
+    <NuxtLink to="/">Back to Mountains</NuxtLink>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ params, redirect }) {
+    const mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then((res) => res.json())
+
+    console.log('filtered Mountain', params, redirect)
+
+
+    const filteredMountain = mountains.find(
+      (el) =>
+        el.continent.toLowerCase() === params.continent &&
+        el.slug === params.mountain
+    )
+    if (filteredMountain) {
+      return {
+        continent: filteredMountain.continent,
+        mountain: filteredMountain.title,
+        image: filteredMountain.image
+      }
+    } else {
+      redirect('/')
+    }
+  }
+}
+</script>
+<style>
+img {
+  width: 25vw;
+  height: 20vw;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,27 @@
 <template>
   <div>
-    <h1>Hello Nuxters! 👋</h1>
-    <p>
-      This page is rendered on the <strong>{{ rendering }}</strong>
-    </p>
-    <p v-if="rendering === 'server'">
-      First load or hard refresh is done on server side.
-    </p>
-    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
-    <ul>
-      <li>Refresh the page for server side rendering.</li>
-      <li>Click the links to see client side rendering.</li>
+    <img src="~/assets/logo.svg" />
+    <ul v-for="mountain in mountains" :key="mountain.id">
+      <NuxtLink :to="`${mountain.continent.toLowerCase()}/${mountain.slug}`">
+        <li>{{ mountain.title }}</li>
+      </NuxtLink>
     </ul>
-
-    <NuxtLink to="/about">About Page
-    </NuxtLink>
   </div>
 </template>
 <script>
 export default {
-  asyncData () {
-    return {
-      rendering: process.server ? 'server' : 'client'
-    }
+  async asyncData() {
+    const mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then((res) => res.json())
+    console.log(mountains)
+    return { mountains }
   }
 }
 </script>
+<style>
+img {
+  width: 20vw;
+  height: 15vw;
+}
+</style>
