766c55bf78504bcaa27135770414cfcb78d34fc1
scm-wutyitheint <scm.dott.developer10@gmail.com>
2022-08-15 13:44:57 +0630

    fetch data at component level

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,8 @@
 <template>
   <div>
     <img src="~/assets/logo.svg" />
-    <h1>Data fetched using asyncData</h1>
-    <ul>
-      <li v-for="mountain in mountains" :key="mountain.title">
-        <NuxtLink
-          :to="{ name: 'mountains-slug', params: { slug: mountain.slug } }"
-        >
-          {{ mountain.title }}
-        </NuxtLink>
-      </li>
-    </ul>
+    <h1>Data fetched using fetch hook</h1>
+    <p>Fetching is at component level</p>
+    <Mountains />
   </div>
 </template>
-<script>
-export default {
-  async asyncData({ $http }) {
-    const mountains = await $http.$get('https://api.nuxtjs.dev/mountains')
-    return { mountains }
-  }
-}
-</script>
